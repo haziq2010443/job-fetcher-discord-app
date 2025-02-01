86d4b014a5c098b974c9d86d25eb85ae3aadf871
Haziq <haziqiskandar443@gmail.com>
2025-02-01 14:28:08 +0800

    created command for job searching. Initialized jobFetcher script

--- commands.js
+++ commands.js
@@ -1,49 +1,22 @@
-import 'dotenv/config';
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
+import 'dotenv/config'
+import { InstallGlobalCommands } from './utils.js'
 
-// Get the game choices from game.js
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
+const { ApplicationCommandType } = require('discord.js')
 
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
-
-  return commandChoices;
-}
-
-// Simple test command
-const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 1, 2],
-};
-
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
+// command to search available job using keyword
+const COMMAND = {
+  name: 'search',
+  description: "Search for jobs based on a keyword",
+  type: ApplicationCommandType.ChatInput,
   options: [
     {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
-    },
-  ],
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 2],
-};
+      name: 'keyword',
+      type: 3, // String type
+      description: "Enter job keyword",
+      required: true
+    }
+  ]
+}
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
-
-InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
+InstallGlobalCommands(process.env.APP_ID, COMMAND)
+module.exports = { COMMAND }
